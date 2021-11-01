65a43b485af28a46b4021c69cf3d7833d70dacfe
Scott Eisele <eiselesr@gmail.com>
2021-11-01 16:29:33 +0000

    update influx 2 example

--- influx2/main.py
+++ influx2/main.py
@@ -1,18 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import datetime
 import time
-
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 from influxdb_client.client.exceptions import InfluxDBError
-
 import yaml
-
-
 
 class TestLogger:
     def __init__(self, influx_config_file):
@@ -22,41 +13,71 @@ class TestLogger:
             except yaml.YAMLError as exc:
                 print(exc)
 
-        print(db_config)
-        # self.db_name = db_config['db_name']
-        # self.db_drop = db_config['db_drop']
-        self.point_values = []
-        # try:
         self.client = influxdb_client.InfluxDBClient(
             url=db_config["url"],
             token=db_config["token"],
             org=db_config["org"]
         )
 
+        print(db_config)
+
         self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
+
+        p = influxdb_client.Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
+        self.write_api.write(bucket=db_config["bucket"],
+                             org=db_config["org"],
+                             record=p)
+
         self.query_api = self.client.query_api()
+        query = f'from(bucket:"{db_config["bucket"]}")\
+        |> range(start: -10m)\
+        |> filter(fn:(r) => r._measurement == "my_measurement")\
+        |> filter(fn: (r) => r.location == "Prague")\
+        |> filter(fn:(r) => r._field == "temperature" )'
+        result = self.query_api.query(org=db_config["org"], query=query)
+        results = []
+        for table in result:
+            for record in table.records:
+                results.append((record.get_field(), record.get_value()))
+
+        print(results)
+
 
         t1 = datetime.datetime.now(tz=datetime.timezone.utc)
         time.sleep(1)
         t2 = datetime.datetime.now(tz=datetime.timezone.utc)
 
-        self.write_api.write("default", "RIAPS", [{"measurement": "h2o_feet", "tags": {"location": "coyote_creek"},
-                                                   "fields": {"water_level": 2.0}, "time": t1},
-                                                  {"measurement": "h2o_feet", "tags": {"location": "coyote_creek"},
-                                                   "fields": {"water_level": 3.0}, "time": t2}])
+        point1 = {"measurement": "h2o_feet",
+                  "tags": {"location": "coyote_creek"},
+                  "fields": {"water_level": 2.0},
+                  "time": t1}
 
+        point2 = {"measurement": "h2o_feet",
+                  "tags": {"location": "coyote_creek"},
+                  "fields": {"water_level": 3.0},
+                  "time": t2}
 
+        points = [point1, point2]
 
-        # self.client = InfluxDBClient(host=db_config['db_host'], port=db_config['db_port'],
-        #                              database=db_config['db_name'], username=db_config['db_user'],
-        #                              password=db_config['db_password'])
-        # self.client.switch_database(db_config['db_name'])
-        # except:
-        #     self.logger.error('database connection failed')
-        #     self.client = None
+        self.write_api.write(bucket=db_config["bucket"], org=db_config["org"], record=points)
+        #
+        query = f'from(bucket:"{db_config["bucket"]}")\
+        |> range(start: -10m)\
+        |> filter(fn: (r) => r._measurement == "h2o_feet")\
+        |> filter(fn: (r) => r.location == "coyote_creek")\
+        |> filter(fn: (r) => r._field == "water_level" )'
 
+        result = self.query_api.query(org=db_config["org"],
+                                      query=query)
+
+        results = []
+        for table in result:
+            for record in table.records:
+                results.append((record.get_value(), record.get_field()))
+
+        print(results)
 
 TestLogger("./cfg/influxdb_config.yaml")
-
-
-
+#
+#
+#
--- opal-rt/udp_test.py
+++ opal-rt/udp_test.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 import scapy
+import select
 import struct
 
 
@@ -8,35 +9,66 @@ import struct
 if __name__ == '__main__':
     print('PyCharm')
 
-    # value = struct.unpack('<d', b'\xeb\x47\x4c\x28\xb4\xbb\x99\x3f')[0]  # little-endian
-    # print(value)
 
     s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s1.settimeout(1)
-    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s2.settimeout(1)
-
-    # s1.bind(("10.1.4.244", 23002))
     s1.bind(("224.0.0.1", 23002))
-    # s2.bind(("10.1.4.244", 25000))
-    # s2.bind(("224.0.0.1", 25000))
 
-    while True:
+    timeout = 100
+    ready_sockets, _, _ = select.select(
+        [s1], [], [], timeout)
+
+
+    while ready_sockets:
         try:
-            data1, addr1 = s1.recvfrom(8)
-            value1 = struct.unpack('<d', data1)  # little-endian
-            print(f"value1: {value1} from: {addr1}")
+            data, addr = s1.recvfrom(32)
+            header = struct.unpack('<Q', data[0:8])
+
+            if header[0] == 18911916:
+                print("valid header")
+
+                try:
+                    value = struct.unpack('<QQQQ', data)  # little-endian
+                except struct.error as e:
+                    print(e)
+                    continue
+
+                connected = value[1]
+
+                if connected != 0 and connected != 1:
+                    print(f"invalid breaker status")
+                    continue
+
+                sender_id = value[2]
+                # sender_id = 123
+
+                if sender_id not in [131]:
+                    print(f"invalid breaker id: {sender_id}")
+                    continue
+
+
+                timestamp = value[3]
+                ip = addr[0]
+                port = addr[1]
+                print(f"relay: {sender_id} status: {connected} from {ip}:{port} time:{timestamp}")
+                ready_sockets, _, _ = select.select(
+                    [s1], [], [], timeout)
+
         except socket.timeout:
             print("No data on s1")
-        # try:
-        #     data2, addr2 = s2.recvfrom(16)
-        #     value2 = struct.unpack('<xxxxxxxxd', data2)  # little-endian
-        #     print(f"value2: {value2} from: {addr2}")
-        except socket.timeout:
-            print("No data on s2")
 
-        # print(f"data1: {data1}")
-        # print(f"data2: {data2}")
+    print(f"No data in {timeout} seconds. Close listener.")
+
+
+
+    #
+    # while True:
+    #     try:
+    #         data1, addr1 = s1.recvfrom(8)
+    #         value1 = struct.unpack('<d', data1)  # little-endian
+    #         print(f"value1: {value1} from: {addr1}")
+    #     except socket.timeout:
+    #         print("No data on s1")
 
 
 
