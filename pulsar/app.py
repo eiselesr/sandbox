7f4b574b37e144d92fdf1694f20b92841b716354
Scott Eisele <eiselesr@gmail.com>
2022-07-13 21:05:02 +0000

    refactor to remove dependency on pulsar from app.py

--- pulsar/app.py
+++ pulsar/app.py
@@ -4,28 +4,20 @@ import threading
 
 class App:
     def __init__(self):
-        self.active = True
         self.msg_count = 0
         print(f"id of function: {id(self.app_function)}")
 
         self.pulsar_client = shim.Shim(self.app_function)
 
-        self.messages_received = threading.Event()
-
-    def app_function(self, consumer, msg):
+    def app_function(self, msg):
         self.msg_count += 1
         print(f"Received message {msg.data()} "
               f"id={msg.message_id()} "
               f"msg_count={self.msg_count}")
-        # Acknowledge successful processing of the message
-        consumer.acknowledge_cumulative(msg)
-        if self.msg_count >= 10:
-            # a.active = False
-            a.messages_received.set()
 
 
 if __name__ == '__main__':
     a = App()
 
-    a.messages_received.wait()
+
 
--- pulsar/shim.py
+++ pulsar/shim.py
@@ -1,15 +1,30 @@
 import logging
 import os
 import pulsar
+import threading
 
 
 class Shim:
     def __init__(self, func):
         print(f"id of function: {id(func)}")
+        self.messages_received = threading.Event()
+        self.app_function = func
         self.client = pulsar.Client('pulsar://localhost:6650')
         self.consumer = self.client.subscribe('my-topic',
                                               'my-subscription',
-                                              message_listener=func)
+                                              message_listener=self.shim_function)
+
+        self.cleanup_channel = self.client.subscribe('cleanup-topic',
+                                                     'cleanup-subscription',
+                                                     message_listener=self.cleanup)
+        self.messages_received.wait()
+
+    def cleanup(self, consumer, msg):
+        self.messages_received.set()
+
+    def shim_function(self, consumer, msg):
+        self.app_function(msg)
+        consumer.acknowledge_cumulative(msg)
 
     def close(self):
         self.client.close()
